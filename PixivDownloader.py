11f8a9497c61dbd643276991c3293d1ae3d7fcdc
yo1993 <yo021830@gmail.com>
2019-06-19 07:04:06 +0900

    ファイルを整理

---
+++ PixivDownloader.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Pixivの画像をダウンロード
+メンバーID・イラストID・タグ　を指定して画像保存
+"""
+
+
+# ==== ライブラリ ====
+
+from pixivpy3 import *
+import json
+from time import sleep
+import os
+
+
+# ====定数 ====
+
+
+
+# ==== ここからスタート ====
+if __name__ == '__main__':
+
+    print("start")
+
+    # ログイン
+
+
+
+    print("end")
---
+++ README.md
@@ -0,0 +1,7 @@
+# Pixiv画像ダウンローダ
+## 概要
+ - メンバーID・イラストID・タグ　を指定して画像保存  
+ 
+ ## 参考
+ - [Pixiv API for Python](https://github.com/upbit/pixivpy)  
+ 
